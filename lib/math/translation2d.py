32be51de3b72746730b32ccaabaeedd2763efd43
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-10-29 14:44:00 +0000

    Refactor Transform2d and Translation2d classes

--- lib/math/translation2d.py
+++ lib/math/translation2d.py
@@ -6,12 +6,12 @@ from .rotation2d import Rotation2d
 
 @dataclass
 class Translation2d:
-    m_x: float
-    m_y: float
+    x: float
+    y: float
 
     def __init__(self, x: float = 0.0, y: float = 0.0):
-        self.m_x = x
-        self.m_y = y
+        self.x = x
+        self.y = y
 
     @classmethod
     def from_distance_and_angle(cls, distance: float, angle: Rotation2d):
@@ -20,66 +20,66 @@ class Translation2d:
         return cls(x, y)
 
     def get_distance(self, other: 'Translation2d') -> float:
-        return math.hypot(other.m_x - self.m_x, other.m_y - self.m_y)
+        return math.hypot(other.x - self.x, other.y - self.y)
 
     def get_x(self) -> float:
-        return self.m_x
+        return self.x
 
     def get_y(self) -> float:
-        return self.m_y
+        return self.y
 
     def to_vector(self):
-        return [self.m_x, self.m_y]
+        return [self.x, self.y]
 
     def get_norm(self) -> float:
-        return math.hypot(self.m_x, self.m_y)
+        return math.hypot(self.x, self.y)
 
     def get_angle(self) -> Rotation2d:
-        return Rotation2d(self.m_x, self.m_y)
+        return Rotation2d(self.x, self.y)
 
     def rotate_by(self, other: Rotation2d) -> 'Translation2d':
         return Translation2d(
-            self.m_x * other.get_cos() - self.m_y * other.get_sin(),
-            self.m_x * other.get_sin() + self.m_y * other.get_cos()
+            self.x * other.get_cos() - self.y * other.get_sin(),
+            self.x * other.get_sin() + self.y * other.get_cos()
         )
 
     def plus(self, other: 'Translation2d') -> 'Translation2d':
-        return Translation2d(self.m_x + other.m_x, self.m_y + other.m_y)
+        return Translation2d(self.x + other.x, self.y + other.y)
 
     def minus(self, other: 'Translation2d') -> 'Translation2d':
-        return Translation2d(self.m_x - other.m_x, self.m_y - other.m_y)
+        return Translation2d(self.x - other.x, self.y - other.y)
 
     def unary_minus(self) -> 'Translation2d':
-        return Translation2d(-self.m_x, -self.m_y)
+        return Translation2d(-self.x, -self.y)
 
     def times(self, scalar: float) -> 'Translation2d':
-        return Translation2d(self.m_x * scalar, self.m_y * scalar)
+        return Translation2d(self.x * scalar, self.y * scalar)
 
     def div(self, scalar: float) -> 'Translation2d':
-        return Translation2d(self.m_x / scalar, self.m_y / scalar)
+        return Translation2d(self.x / scalar, self.y / scalar)
 
     def nearest(self, translations: List['Translation2d']) -> 'Translation2d':
         return min(translations, key=self.get_distance)
 
     def __str__(self) -> str:
-        return f"Translation2d(X: {self.m_x:.2f}, Y: {self.m_y:.2f})"
+        return f"Translation2d(X: {self.x:.2f}, Y: {self.y:.2f})"
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Translation2d):
-            return math.isclose(other.m_x, self.m_x, abs_tol=1E-9) and \
-                   math.isclose(other.m_y, self.m_y, abs_tol=1E-9)
+            return math.isclose(other.x, self.x, abs_tol=1E-9) and \
+                   math.isclose(other.y, self.y, abs_tol=1E-9)
         return False
 
     def __hash__(self) -> int:
-        return hash((self.m_x, self.m_y))
+        return hash((self.x, self.y))
 
     def interpolate(self, end_value: 'Translation2d', t: float) -> 'Translation2d':
-        x = self.m_x + (end_value.m_x - self.m_x) * t
-        y = self.m_y + (end_value.m_y - self.m_y) * t
+        x = self.x + (end_value.x - self.x) * t
+        y = self.y + (end_value.y - self.y) * t
         return Translation2d(x, y)
 
     def to_json(self):
-        return json.dumps({'x': self.m_x, 'y': self.m_y})
+        return json.dumps({'x': self.x, 'y': self.y})
 
     @staticmethod
     def from_json(json_str: str) -> 'Translation2d':
