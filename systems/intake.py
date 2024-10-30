b8fd0a725551923e34cfe4d84e393b38d84b8dcd
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-10-30 18:37:38 +0000

    Refactor Intake class constructor to simplify parameters and set default name

--- systems/intake.py
+++ systems/intake.py
@@ -2,8 +2,8 @@ from lib.actions.action import Action
 from hardware.motor import Motor
 
 class Intake(Action):
-    def __init__(self, name, description, intake, left_motor_port, right_motor_port):
-        super().__init__(name, description)
+    def __init__(self, intake, left_motor_port, right_motor_port):
+        super().__init__("Intake System")
         self.intake = intake
         self.left_motor = Motor(left_motor_port)
         self.right_motor = Motor(right_motor_port)
