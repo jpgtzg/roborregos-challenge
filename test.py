e8cc76929e086b51892d7fd6b02e0cfcc567576c
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-11-03 20:34:02 -0600

    Updated test.py

--- test.py
+++ test.py
@@ -21,7 +21,6 @@ logger.info("Starting test")
 scheduler = ActionScheduler()
 
 chassis_system = Chassis(
-chassis_system = Chassis(
     motor1=Motor(IN1=Constants.ChassisConstants.MOTOR1_IN1, IN2=Constants.ChassisConstants.MOTOR1_IN2, PWM=Constants.ChassisConstants.MOTOR1_PWM, inverted=Constants.ChassisConstants.MOTOR1_INVERTED),
     motor2=Motor(IN1=Constants.ChassisConstants.MOTOR2_IN1, IN2=Constants.ChassisConstants.MOTOR2_IN2, PWM=Constants.ChassisConstants.MOTOR2_PWM, inverted=Constants.ChassisConstants.MOTOR2_INVERTED),
     motor3=Motor(IN1=Constants.ChassisConstants.MOTOR3_IN1, IN2=Constants.ChassisConstants.MOTOR3_IN2, PWM=  Constants.ChassisConstants.MOTOR3_PWM, inverted=Constants.ChassisConstants.MOTOR3_INVERTED),
@@ -29,27 +28,16 @@ chassis_system = Chassis(
 )
 
 
-move_action1 = RunAction("MoveAction", lambda: chassis_system.move(v=-100, omega=1, vel_angle=math.radians(270), robot_theta=0))
+move_action1 = RunAction("MoveAction", lambda: chassis_system.move(v=-50, omega=50, vel_angle=math.radians(270), robot_theta=0))
 move_action2 = RunAction("MoveAction", lambda: chassis_system.move(v=-100, omega=1, vel_angle=math.radians(180), robot_theta=0))
 move_action3 = RunAction("MoveAction", lambda: chassis_system.move(v=-100, omega=1, vel_angle=math.radians(90), robot_theta=0))
 move_action4 = RunAction("MoveAction", lambda: chassis_system.move(v=-100, omega=1, vel_angle=math.radians(0), robot_theta=0))
 
 final_action = InstantAction(lambda: chassis_system.stop())
 
-complete_action= move_action1.withTimeout(5).andThen(move_action2.withTimeout(5)).andThen(move_action3.withTimeout(5)).andThen(move_action4.withTimeout(5)).andThen(final_action)
+complete_action= move_action1.withTimeout(1).andThen(move_action2.withTimeout(1)).andThen(move_action3.withTimeout(1)).andThen(move_action4.withTimeout(1)).andThen(final_action)
 
 scheduler.schedule_action(complete_action)
-    motor4=Motor(IN1=Constants.ChassisConstants.MOTOR4_IN1, IN2=Constants.ChassisConstants.MOTOR4_IN2, PWM=Constants.ChassisConstants.MOTOR4_PWM, inverted=Constants.ChassisConstants.MOTOR4_INVERTED)
-)
-
-
-#from lib.math.robot_kinematics import Kinematics
-
-move_action = RunAction("MoveAction", lambda: chassis_system.move(100, 0, 0))
-
-printAction = RunAction("Prin Action", lambda: print("HII"))
-
-#scheduler.schedule_action()
 
 import math
 while True:
@@ -59,19 +47,10 @@ while True:
     if complete_action.is_finished():
         break
 """ printAction = InstantAction("PrintAction", lambda: print("Hello World!"))
-""" while True:
-    #scheduler.run() """
 
-waitAction = WaitAction(1) # Finishes after 1 second
-
-printAction = InstantAction("PrintAction", lambda: print("Hello World!")) # Prints Hello World! Once
-
-parallelAction = waitAction.alongWith(printAction) # Waits for 1 second and prints Hello World! at the same time
-sequentialAction = waitAction.andThen(printAction) # Waits for 1 second and then prints Hello World!
+parallelAction = SequentialActionGroup(waitAction, printAction)
 
 scheduler.schedule_action(parallelAction)
 
 while True:
-    scheduler.run()
-
-
+    scheduler.run() """
